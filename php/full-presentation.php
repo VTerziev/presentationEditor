0412a20094621403043a8a897b9d7874d39ed10c
Polly <paolina.gadjulova@leanplum.com>
2022-02-14 21:10:06 +0200

    download wip

--- php/full-presentation.php
+++ php/full-presentation.php
@@ -1,5 +1,17 @@
 <?php
-    $data = ["presentation" => "= slide 'Използване на CSS' do
+    include_once 'config/database.php';
+    include_once 'models/files.php';
+
+    $database = new Database();
+    $db = $database->connect();
+
+    // $file = new File($db);
+    // $file->fileName = $_POST['presentation'];
+    // $file->read_single();
+
+	// echo json_encode($_POST);
+
+	$data = ["presentation" => "= slide 'Използване на CSS' do
     p Има 3 начина на използване на CSS:
     list:
     като стойност на style атрибута на html елемент (very bad) <xmp><a href=\"//google.com\" style=\"color:red\">Click Here</a></xmp>
@@ -7,4 +19,5 @@
     като го линк-нете като външен файл (good) <xmp><link href=\"style.css\" rel=\"stylesheet\"></xmp>
     "];
     echo json_encode($data);
+
 ?>
